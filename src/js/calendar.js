591a9ea10f9dee80f16844392dd641b9306cebb8
pzyryanov <pzyryanov1995@gmail.com>
2018-07-03 16:54:48 +0700

    add disabling for passed date

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -1,16 +1,42 @@
+var HOUR_IN_DAY = 24;
+var MIN_IN_HOUR = 60;
+var SEC_IN_MIN = 60;
+var MS_IN_SEC = 1000;
+var DAY_IN_MS = HOUR_IN_DAY * MIN_IN_HOUR * SEC_IN_MIN * MS_IN_SEC;
+
 var eventsObject = events[0];
 
-var createCalendarItemDom = function(data, month) {
-    var template = $('#cal-slider__item')[0].content;
-    var itemTemplate = $(template).clone();
-    itemTemplate.find('.cal__container-body').html(data);
-    itemTemplate.find('.cal-slider__item-title').html(month);
-    var fragment = $(document.createDocumentFragment());
-    fragment.append(itemTemplate);
-    // $('#cal-slider').after(fragment);
-    $(fragment).appendTo($('#cal-slider'));
+var createCalendarItemDom = function (data, month) {
+	var template = $('#cal-slider__item')[0].content;
+	var itemTemplate = $(template).clone();
+	itemTemplate.find('.cal__container-body').html(data);
+	itemTemplate.find('.cal-slider__item-title').html(month);
+	var fragment = $(document.createDocumentFragment());
+	fragment.append(itemTemplate);
+	$(fragment).appendTo($('#cal-slider'));
 }
 
+var getCurrentDate = function () {
+	var today = new Date();
+	var todayInMs = today.getTime(); // current time in ms
+	return todayInMs;
+}
+var todayInMs = getCurrentDate();
+
+var checkDateIsPass = function (dateTime) {
+	var dateForCheck = dateTime + DAY_IN_MS; // for correlation in 1 day
+	if (dateForCheck < todayInMs) {
+		return true;
+	}
+	return false;
+}
+
+var addDisabledClassForPassedDate = function (dateTime) {
+	if (checkDateIsPass(dateTime) === true) {
+		return "disabled";
+	}
+	return "";
+}
 
 var createCalendarItem = function(year) {
     var yearEvents = window.eventsObject.year[year];
@@ -29,8 +55,9 @@ var createCalendarItem = function(year) {
             if(dayEvents[prop].text !== '') {
                 hasEvent = true;
             }
+            var dateTime = new Date(year, month - 1, prop, 0, 0, 0, 0).getTime();
             html += 
-            '<div class="cal__item" data-events=' + hasEvent + ' data-date=' + year + '.' + month + '.' + prop +'>' +
+            '<div class="cal__item ' + addDisabledClassForPassedDate(dateTime) +'" data-events=' + hasEvent +' data-date=' + year + '.' + month + '.' + prop +'>' +
                 '<div class="cal__item-date">' +
                     prop + 
                 '</div>' +
